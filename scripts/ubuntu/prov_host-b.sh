208ef9c8f688fb15ca36e434c09fe94289ee8cf6
as3ii <as3ii777@gmail.com>
2019-10-29 16:03:12 +0100

    fixed routing, added docker and nginx container on host-c

--- scripts/ubuntu/prov_host-b.sh
+++ scripts/ubuntu/prov_host-b.sh
@@ -4,5 +4,5 @@
 ip link set dev enp0s8 up
 ip addr add 10.0.2.2/23 dev enp0s8
 
-ip route add default via 10.0.2.1
+ip route add 10.0.0.0/23 via 10.0.2.1
 
--- scripts/ubuntu/prov_host-c.sh
+++ scripts/ubuntu/prov_host-c.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+# install and enable docker-ce
+curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
+sh /tmp/get-docker.sh
+
 # router-south-2
 ip link set enp0s8 up
 ip addr add 10.0.0.2/23 dev enp0s8
 
-ip route add default via 10.0.0.1
+ip route add 10.0.4.0/25 via 10.0.0.1
+ip route add 10.0.2.0/23 via 10.0.0.1
+
+# run detached nginx-test docker container
+docker run -d -p 80:80 dustnic82/nginx-test
 
